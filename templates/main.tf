55e4ffb80f9167e31df79cb6204a0fbc29170354
Max Cotton <mcotton@mxnet.ca>
2022-09-19 10:02:55 -0400

    feat: update template with auto-caching

--- templates/main.tf
+++ templates/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.4.9"
+      version = "0.4.12"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.20.2"
+      version = "~> 2.21.0"
     }
   }
 }
@@ -20,13 +20,36 @@ provider "docker" {
 data "coder_workspace" "me" {
 }
 
+data "docker_registry_image" "image" {
+  name = "ghcr.io/mxnet-dev/${var.image_version}"
+}
+
+resource "docker_image" "image" {
+  name          = "${trim(data.docker_registry_image.image.name, "1234567890:")}@${data.docker_registry_image.image.sha256_digest}"
+  pull_triggers = [data.docker_registry_image.image.sha256_digest]
+  keep_locally  = true
+}
+
 variable "dotfiles_uri" {
   description = <<-EOF
   Dotfiles repo URI (optional)
-
-  see https://dotfiles.github.io
   EOF
   default     = "https://github.com/mxnet-dev/dotfiles"
+}
+
+variable "image_version" {
+  type        = string
+  description = <<-EOF
+  Which version do you want to use for your workspace?
+
+  EOF
+
+  default = "ubuntu:2204"
+
+  validation {
+    condition     = contains(["ubuntu:2004","ubuntu:2204"], var.image_version)
+    error_message = "Value must be ubuntu:2004, ubuntu:2204."
+  }
 }
 
 resource "coder_agent" "main" {
@@ -38,7 +61,20 @@ resource "coder_agent" "main" {
   }
   arch           = data.coder_provisioner.me.arch
   os             = "linux"
-  startup_script = var.dotfiles_uri != "" ? "coder dotfiles -y ${var.dotfiles_uri}" : null
+  dir            = "/home/coder"
+  startup_script = <<EOF
+  #!/bin/sh
+  %{ if var.dotfiles_uri != "" }coder dotfiles -y ${var.dotfiles_uri}%{ endif }
+  curl -fsSL https://code-server.dev/install.sh | sh
+  code-server --auth none --port 13337 &
+  EOF
+}
+
+resource "coder_app" "code-server" {
+  agent_id = coder_agent.main.id
+  name     = "VSCode"
+  icon     = "/icon/code.svg"
+  url      = "http://localhost:13337/?folder=/home/coder"
 }
 
 resource "docker_volume" "home_volume" {
@@ -46,17 +82,15 @@ resource "docker_volume" "home_volume" {
 }
 
 resource "docker_container" "workspace" {
-  count = data.coder_workspace.me.start_count
-  image = "codercom/enterprise-base:ubuntu"
-  # Uses lower() to avoid Docker restriction on container names.
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  dns  = ["1.1.1.1"]
-  # Refer to Docker host when Coder is on localhost
+  count   = data.coder_workspace.me.start_count
+  image   = docker_image.image.image_id
+  name    = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+  dns     = ["1.1.1.1"]
   command = ["sh", "-c", replace(coder_agent.main.init_script, "127.0.0.1", "host.docker.internal")]
   env     = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
   host {
-    host = "host.docker.internal"
-    ip   = "host-gateway"
+    host  = "host.docker.internal"
+    ip    = "host-gateway"
   }
   volumes {
     container_path = "/home/coder/"
@@ -70,7 +104,28 @@ resource "coder_metadata" "container_info" {
   resource_id = docker_container.workspace[0].id
 
   item {
+    key   = "arch"
+    value = coder_agent.main.arch
+  }
+
+  item {
+    key   = "name"
+    value = docker_container.workspace[0].name
+  }
+
+  item {
+    key   = "version"
+    value = var.image_version
+  }
+}
+
+resource "coder_metadata" "image_info" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_image.image.id
+  hide        = false
+
+  item {
     key   = "image"
-    value = docker_container.workspace[0].image
+    value = docker_image.image.name
   }
 }
